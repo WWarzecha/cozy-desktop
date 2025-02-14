28546741215dd8480829b54053f18272fe85937d
Woodenspoon <wmwarzecha@gmail.com>
2025-02-14 19:52:18 +0100

    add resize beta option

--- src/windows/app-window.js
+++ src/windows/app-window.js
@@ -15,6 +15,36 @@ const addTopBar = (dom) => {
     return topBar;
 };
 
+const onVerticalEdge = (dom) => {
+    let isOnEdge, resizable = false;
+    let lastX, lastY = 0;
+    dom.addEventListener("mousemove", (e) => {
+        if(!isOnEdge && Math.abs(e.clientX - dom.offsetLeft) < 8){
+            isOnEdge = true;
+            document.body.style.cursor = "e-resize";
+        }
+        else if(isOnEdge && Math.abs(e.clientX - dom.offsetLeft) > 8 && !resizable){
+            isOnEdge = false;
+            document.body.style.cursor = "default";
+        }
+    });
+    document.addEventListener("mousemove", (e) => {
+        if(isOnEdge && resizable){
+            dom.style.left = `${e.clientX}px`;
+            dom.style.width = `${parseInt(dom.style.width) - e.clientX + lastX}px`
+            console.log(e.clientX - dom.offsetLeft);
+            lastX = e.clientX;
+        }
+    });
+    dom.addEventListener("mousedown", (e) => {if(isOnEdge) resizable = true; lastX = e.clientX});
+    document.addEventListener("mouseup", (e) => {if(resizable) resizable = false});
+    dom.addEventListener("mouseleave", (e) => {
+        if(isOnEdge && !resizable){
+            document.body.style.cursor = "default";
+            isOnEdge = false;
+        }
+    });
+}
 
 const AppWindow = class {
     constructor(width, height) {
@@ -23,6 +53,7 @@ const AppWindow = class {
         this.topBar = addTopBar(this.dom);
         this.isDragged = false;
         this.#makeDraggable();
+        onVerticalEdge(this.dom);
     };
     #makeDraggable() {
         let offsetX, offsetY, isDragging = false;
